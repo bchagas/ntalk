8c91c2926f652bfd2deb74b7f3e7493ffeb01701
Bruno Chagas <brunochagasweb@gmail.com>
2013-08-13 20:07:15 -0300

    Fixed minor issues

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
   var contacts = app.controllers.contacts;
   app.get('/contacts', contacts.index);
   app.get('/contacts/:id', contacts.show);
-  app.post('/contacts', contacts.create);
+  app.post('/contact', contacts.create);
   app.get('/contacts/:id/editar', contacts.edit);
   app.put('/contacts/:id', contacts.update);
   app.del('/contacts/:id', contacts.destroy);
